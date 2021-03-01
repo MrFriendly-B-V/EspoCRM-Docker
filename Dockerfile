741693c097b598b775e833af0c26a07734b05b85
TheDutchMC <github@thedutchmc.nl>
2021-03-01 10:27:48 +0100

    Switched to using own Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,55 @@
+FROM ubuntu
+
+ARG DEBIAN_FRONTEND=noninteractibe
+
+#Update and required dependency for adding a repository
+RUN apt-get update -y
+RUN apt-get install -y software-properties-common
+
+#Add the PHP repository and update
+RUN add-apt-repository -y ppa:ondrej/php
+RUN apt-get update -y
+
+#Install required dependencies
+RUN apt-get install -y --no-install-recommends \
+        php7.4 \
+        php7.4-mysql \
+        php7.4-json \
+        php7.4-gd \
+        php7.4-zip \
+        php7.4-imap  \
+        php7.4-mbstring \
+        php7.4-curl \
+        php7.4-exif \
+        php7.4-xml \ 
+        php7.4-xmlwriter \
+        php7.4-posix \
+        php7.4-zmq \
+        php7.4-fpm \
+        nginx \
+        wget \
+        unzip \
+        cron \
+        supervisor
+
+#Enable PHP modules
+RUN phpenmod imap mbstring
+
+#Crontab
+RUN echo "$(echo '* * * * * /usr/bin/php -f /var/www/html/espocrm/cron.php > /dev/null 2>&1' ; crontab -u root -l)" | crontab -u root -
+
+#Delete default nginx configs
+RUN rm -rf /etc/nginx/sites-available/*
+RUN rm -rf /etc/nginx/sites-enabled/*
+
+#Copy in our nginx config and symlink it
+COPY ./espocrm.conf /etc/nginx/sites-available/espocrm.conf 
+RUN ln -s /etc/nginx/sites-available/espocrm.conf /etc/nginx/sites-enabled/espocrm.conf
+
+#Copy in our script and the supervisord configuration file
+COPY ./run.sh /app/run.sh
+COPY ./supervisord.conf /app/supervisord.conf
+
+EXPOSE 80
+
+ENTRYPOINT ["sh", "-c", "/app/run.sh"]
---
+++ run.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#If EspoCRM is not installed, we need to install it
+if [ -z "$(ls -A /var/www/espocrm)" ]
+then
+    echo "EspoCRM not installed. Installing..."
+
+    wget https://www.espocrm.com/downloads/EspoCRM-6.1.4.zip -O /tmp/espocrm.zip
+    unzip /tmp/espocrm.zip -d /tmp/espocrm
+    mv /tmp/espocrm/EspoCRM-6.1.4/* /var/www/espocrm
+
+    cd /var/www/espocrm
+    chown -R www-data:www-data /var/www/espocrm
+    find . -type d -exec chmod 755 {} + && find . -type f -exec chmod 644 {} +;
+    find data custom client/custom -type d -exec chmod 775 {} + && find data custom client/custom -type f -exec chmod 664 {} +;
+    chmod 775 application/Espo/Modules client/modules;
+
+    echo "EspoCRM is now installed."
+else
+    echo "EspoCRM is already installed. Continueing"
+fi
+
+echo "Running preflight configuration..."
+
+#Create the files and folders required for PHP FPM
+mkdir -p /run/php/
+touch /run/php/php7.4-fpm.sock
+mkdir -p /var/log/php-fpm
+
+#Configure php.ini
+sed -i 's/max_execution_time = .*/max_execution_time = 180/g' /etc/php/7.4/fpm/php.ini
+sed -i 's/max_input_time = .*/max_input_time = 180/g' /etc/php/7.4/fpm/php.ini
+sed -i 's/post_max_size = .*/post_max_size = 20M/g' /etc/php/7.4/fpm/php.ini
+sed -i 's/memory_limit = .*/memory_limit = 256M/g' /etc/php/7.4/fpm/php.ini
+sed -i 's/upload_max_filesize = .*/upload_max_filesize = 20M/g' /etc/php/7.4/fpm/php.ini
+
+echo "Configuration complete"
+
+/usr/bin/supervisord -n -c /app/supervisord.conf
